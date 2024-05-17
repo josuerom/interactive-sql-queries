8464e9f03b4137ae28fc91cb32a19224d727806b
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-05-16 19:36:49 -0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@
 **/
 public class Main {
    public static void main(String[] args) {
-      ConsultarBD con = new ConsultarBD("3306", "prueba", "root", "Jo320872.");
-      // con.enviarConsulta();
-      System.out.println("HOLA!");
+      ConsultarBD con = new ConsultarBD("3306", "prueba", "", "");
+      con.enviarConsulta();
    }
 }
